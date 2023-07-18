44195f61cb246eb43da21ced6768fa79bfc537d2
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2023-07-19 02:27:38 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-// import Login from "./pages/Login";
-// import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 import Register from "./pages/Register";
 import './style.scss';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return(
-    <Register></Register>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
