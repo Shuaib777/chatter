da5c2733e7911cb26992e0408bc558d5e63d4efd
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2023-07-22 18:02:09 +0530

    cha

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,17 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../context/authContext'
+import { onValue, orderByChild, query } from 'firebase/database';
+import { database, refdb } from '../firebase';
+import { ChatContext } from '../context/chatContext';
 
 const Chats = () => {
   const {currentUser} = useContext(AuthContext);
+  const {dispatch, data} = useContext(ChatContext);
+
+  const [chats, setChats] = useState([]);
+
+  useEffect(()=>{
+    const getChats = ()=>{
+      const unsub = onValue(refdb(database, 'userChats/'+ currentUser.uid),(snapshot) => {
+        setChats(snapshot.val());
+      })
+
+      return ()=> {
+        unsub();
+      }
+    }
+
+    currentUser.uid && getChats()
+    
+  },[currentUser.uid])
+
+  const handleClick = (u) => {
+    dispatch({type: "CHANGE_USER", payload: u});
+  };
+
+  //Object.entries() returns an array with two elements: the first element is the property key, and the second element is the property value
   return (
     <div className="chats">
-      <div className="userChat">
-        <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-        <div className="userChatInfo">
-          <span>Shuaib3</span>
-          <p>hello</p>
+      {chats !== 'start conversation'?Object.entries(chats).map((chat) => 
+        <div className="userChat" key={chat[0]} onClick={() => handleClick(chat[1].userinfo)}>
+          <img src={chat[1].userinfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userinfo.displayname}</span>
+            <p>{chat[1].lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
+      ) : <span style={{color:"red", margin:"10px"}}>Start your first conversation by searching users</span>}
     </div>
   )
 }
