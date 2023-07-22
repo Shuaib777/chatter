6f1870ae6e005d4e5db0532f6abf1a11a1921f11
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2023-07-23 03:06:26 +0530

    modified styles

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,39 +1,41 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Message from './Message';
-import { ChatContext } from '../context/chatContext';
-import { onValue } from 'firebase/database';
-import { database, refdb } from '../firebase';
-import { AuthContext } from '../context/authContext';
+import React, { useContext, useEffect, useState } from "react";
+import Message from "./Message";
+import { ChatContext } from "../context/chatContext";
+import { onValue } from "firebase/database";
+import { database, refdb } from "../firebase";
+import { AuthContext } from "../context/authContext";
 
 const Messages = () => {
-
-  const {currentUser} = useContext(AuthContext);
-  const [messages, setMessages] = useState([])
-  const {data} = useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
 
   useEffect(() => {
     const getMessages = () => {
-      const unsub = onValue(refdb(database, `chats/${data.chatID}/message/`),(snapshot) => {
-        return setMessages(snapshot.val());
-      })
-  
+      const unsub = onValue(
+        refdb(database, `chats/${data.chatID}/message/`),
+        (snapshot) => {
+          return setMessages(snapshot.val());
+        }
+      );
+
       return () => {
         unsub();
-      }
-    }
+      };
+    };
 
     currentUser.uid && getMessages();
-  }, [data.chatID])
-
-  console.log(messages)
+  }, [data.chatID]);
 
   return (
-    <div className="messages">
-      {messages?.map((m) => (
-        <Message message={m} key={m.id} />
-      ))}
+    <div className="backgroundImage">
+      <div className="messages">
+        {messages?.map((m) => (
+          <Message message={m} key={m.id} />
+        ))}
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Messages
+export default Messages;
