625e7bda6e7ccd83638b4249dde3ef0d7fa97eae
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2023-07-23 17:29:00 +0530

    added profile pic popup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
-import { signOut } from 'firebase/auth'
-import React, { useContext } from 'react'
-import { auth } from '../firebase'
-import { AuthContext } from '../context/authContext'
+import { signOut } from "firebase/auth";
+import React, { useContext } from "react";
+import { auth } from "../firebase";
+import { AuthContext } from "../context/authContext";
+import { PopupContext } from "../context/popupContext";
 
 const Navbar = () => {
+  const { dispatch } = useContext(PopupContext);
+
   const { currentUser } = useContext(AuthContext);
-  console.log(currentUser);
+  const handleOpen = () => dispatch({ type: "CHANGE_STATE", payload: true });
   return (
     <div className="navbar">
       <div className="logo">Chatter</div>
       <div className="user">
-        <img src={currentUser.photoURL} alt="" />
+        <img src={currentUser.photoURL} alt="" onClick={() => handleOpen()} />
         <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
