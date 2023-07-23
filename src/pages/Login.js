c6f807bc7fc5aca67e1a7d8462abbd43983d158c
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2023-07-24 00:42:44 +0530

    added forgot password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,43 +1,69 @@
 import React, { useState } from "react";
 import { auth } from "../firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 // import { setDoc, doc } from "firebase/firestore";
 
 const Login = () => {
   const [err, setErr] = useState(false);
+  const [emailAdd, setEmailAdd] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const email = e.target[0].value;
+    setEmailAdd(email);
     const password = e.target[1].value;
 
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate('/');
-
-    } catch (error) { 
-      const errorMessage = error.message.substring(22, error.message.length - 2);
+      navigate("/");
+    } catch (error) {
+      const errorMessage = error.message.substring(
+        22,
+        error.message.length - 2
+      );
       setErr(() => errorMessage);
     }
   };
 
+  const handleForgotPassword = (e) => {
+    sendPasswordResetEmail(auth, emailAdd)
+      .then(() => {
+        alert("Password reset link send to your email address");
+      })
+      .catch((error) => {
+        const errorMessage = error.message.substring(
+          22,
+          error.message.length - 2
+        );
+        setErr(() => errorMessage);
+      });
+  };
+
   return (
-   <div className="formContainer">
-    <div className="formWrapper">
-      <span className="logo">Chatter</span>
-      <span className="title">Login</span>
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder='Email'/>
-        <input type="password" placeholder='Password' />
-        <button>Sign in</button>
-        <p>Not a member?<Link to="/register">Register</Link></p>
-        {err && <span style={{ color: "red" }}>{err}!!</span>}
-      </form>
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chatter</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <p style={{ cursor: "pointer" }} onClick={handleForgotPassword}>
+            Forgot password?
+          </p>
+          <button>Sign in</button>
+          <p>
+            Not a member?<Link to="/register">Register</Link>
+          </p>
+          {err && <span style={{ color: "red" }}>{err}!!</span>}
+        </form>
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
 
   const handlechange = () => {
     setAvatar(true);
-  }
+  };
 
   //The keyword async before a function makes the function return a promise.
   //The await keyword makes the function pause the execution and wait for a resolved promise before it continues.
@@ -36,6 +36,10 @@ const Register = () => {
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
+      // await sendEmailVerification(auth.currentUser).then(() => {
+      //   alert("Email verification Link send to your email address");
+      // });
+
       const storageRef = ref(storage, email);
 
       await uploadBytesResumable(storageRef, file).then(() => {
@@ -48,33 +52,39 @@ const Register = () => {
               photoURL: downloadURL,
             });
 
-            await update(refdb(database, 'users/' + res.user.uid), {
+            await update(refdb(database, "users/" + res.user.uid), {
               uid: res.user.uid,
               displayName,
               email,
               photoURL: downloadURL,
             });
 
-            await update(refdb(database, 'userChats/'), { [res.user.uid] : 'start conversation'});
-            navigate('/');
+            await update(refdb(database, "userChats/"), {
+              [res.user.uid]: "start conversation",
+            });
+            navigate("/");
 
-          //   this is for firestore
-          //   await setDoc(doc(db, "users", res.user.uid), {
-          //     uid: res.user.uid,
-          //     displayName,
-          //     email,
-          //     photoURL: downloadURL,
-          // });
-
-          } catch (error) { 
-              const errorMessage = error.message.substring(22, error.message.length - 2);
-              setErr(() => errorMessage);
-          }  
+            //   this is for firestore
+            //   await setDoc(doc(db, "users", res.user.uid), {
+            //     uid: res.user.uid,
+            //     displayName,
+            //     email,
+            //     photoURL: downloadURL,
+            // });
+          } catch (error) {
+            const errorMessage = error.message.substring(
+              22,
+              error.message.length - 2
+            );
+            setErr(() => errorMessage);
+          }
         });
       });
-
-    } catch (error) { 
-      const errorMessage = error.message.substring(22, error.message.length - 2);
+    } catch (error) {
+      const errorMessage = error.message.substring(
+        22,
+        error.message.length - 2
+      );
       setErr(() => errorMessage);
     }
   };
@@ -88,14 +98,27 @@ const Register = () => {
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <input style={{ display: "none" }} type="file" id="file" onChange={handlechange} />
+          <input
+            style={{ display: "none" }}
+            type="file"
+            id="file"
+            onChange={handlechange}
+          />
           <label htmlFor="file">
             {avatar || <img src={Add} alt="" />}
-            <span>{ avatar ? <img style={{width: "40px"}} src={tick} alt=""></img> : 'Add an avatar' }</span>
+            <span>
+              {avatar ? (
+                <img style={{ width: "40px" }} src={tick} alt=""></img>
+              ) : (
+                "Add an avatar"
+              )}
+            </span>
           </label>
           <button>Sign up</button>
           {err && <span style={{ color: "red" }}>{err}!!</span>}
-          <p>Already a member?<Link to="/login">Login</Link></p>
+          <p>
+            Already a member?<Link to="/login">Login</Link>
+          </p>
         </form>
       </div>
     </div>
