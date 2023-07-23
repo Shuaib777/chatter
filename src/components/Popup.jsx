33abe887cf253a23d8c07629588fda13455806fe
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2023-07-23 20:55:55 +0530

    popup added

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,30 +1,44 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { PopupContext } from "../context/popupContext";
 import { AuthContext } from "../context/authContext";
 import editImage from "../image/pencil.png";
-import { storage } from "../firebase";
+import { database, refdb, storage } from "../firebase";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { update } from "firebase/database";
 
 const Popup = () => {
   const { dispatch } = useContext(PopupContext);
   const { currentUser } = useContext(AuthContext);
-  //   cosnt [profilePic, setProfilePic] = useState(null);
 
   const handleClose = () => dispatch({ type: "CHANGE_STATE", payload: false });
-  //   const handleChange = async () => {
-  //     const storageRef = ref(storage, currentUser.email);
 
-  //     await uploadBytesResumable(storageRef, file).then(() => {
-  //       getDownloadURL(storageRef).then(async (downloadURL) => {});
-  //     });
-  //   };
+  const handleChange = async (file) => {
+    const storageRef = ref(storage, currentUser.email);
+
+    await uploadBytesResumable(storageRef, file).then((snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+
+      getDownloadURL(storageRef).then(async (downloadURL) => {
+        await update(refdb(database, `users/${currentUser.uid}/`), {
+          ["photoURL/"]: downloadURL,
+        });
+        window.location.reload(true);
+      });
+    });
+  };
   return (
     <>
       <div className="wrapper"></div>
       <div className="popup">
         <img src={currentUser.photoURL} className="popupImage" alt="" />
         <button onClick={() => handleClose()}>❌</button>
-        <input type="file" style={{ display: "none" }} id="userImage" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="userImage"
+          onChange={(e) => handleChange(e.target.files[0])}
+        />
         <label htmlFor="userImage">
           <img src={editImage} alt="" />
         </label>
